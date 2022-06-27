bfc0c0228e787702ebdd4eae011cbb3e197e018c
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2022-06-27 16:07:13 -0300

    ex014 feito

--- aulas/aula12ex/EX014/script.js
+++ aulas/aula12ex/EX014/script.js
@@ -1,1 +1,21 @@
-alert('OLA')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML= `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA
+        img.src ='manha250.PNG'
+        document.body.style.background = '#de4e1a'
+    } else if (hora >= 12 && hora <= 18){
+        // Boa tarde
+        img.src = 'tarde250.PNG'
+        document.body.style.background = '#a56152'
+    }else {
+        //Boa noite
+        img.src = 'noite250.PNG'
+        document.body.style.background = '#0268a3'
+    }
+
+}
