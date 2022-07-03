57c2bc52dc6d7d6bc8a6b54f7fca6248018c4c87
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2022-07-03 07:42:54 -0300

    ex 16 feito

--- aulas/aula14ex/ex16/script.js
+++ aulas/aula14ex/ex16/script.js
@@ -1,1 +1,48 @@
-alert('OLA')
+function Contar() {
+  var inicio = document.getElementById("txtInicio");
+  var fim = document.getElementById("txtFim");
+  var passo = document.getElementById("txtPasso");
+  var res = document.getElementById("res");
+  var i = Number(inicio.value)
+  var f = Number(fim.value)
+  var p = Number(passo.value)
+  res.innerHTML = ``;
+
+  if (p <= 0){
+    window.alert ('o passo nao pode ser 0 entao recebe automaticamente 1 ')
+    p = 1
+  }
+
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = ("[ERRO] Por favor adicionar um valor ");
+    return;
+  }
+
+  if (i <= f) {
+    console.log ('inicio <= fim ')
+
+    for (
+      c = i;
+      c <= f;
+      c = c + p
+    ) {
+      res.innerHTML += `${c} \u{1F449}`;
+    }
+    res.innerHTML += ` \u{1F3C1} `;
+  } else {
+  console.log ('inicio >= fim')
+    for (
+      c = i;
+      c >= f;
+      c = c - p
+    ) {
+      res.innerHTML += `${c} \u{1F449}`;
+    }
+    res.innerHTML += ` \u{1F3C1} `;
+  }
+}
+ 
---
+++ teste/passo/passo.js
@@ -0,0 +1,13 @@
+inicio = 10
+fim = 2
+passo = 1
+if (inicio <= fim) {
+for (c = inicio;c <= fim ;c = c + passo){
+    console.log(c)
+}
+}
+else {
+    for (c = inicio;c >= fim ;c = c -  passo){
+        console.log(c)
+    }
+}
