43151ef8274b52481a3dea7772f34f55631381c7
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2022-07-03 16:46:13 -0300

    ex 17 feito

--- aulas/aula14ex/ex17/script.js
+++ aulas/aula14ex/ex17/script.js
@@ -1,1 +1,18 @@
-alert('OLA')
+function tabuada(){
+var tab = document.getElementById('seltab')
+var numero = document.getElementById('txtn')
+var n = Number(numero.value)
+if(numero.value.length == 0 ){
+    window.alert('porfavor digite um numero ')
+    return;
+}
+tab.innerHTML = ' '
+for (c=1;c<=10;c++) {
+  var item = document.createElement('option')
+  item.text = `${n} x ${c} = ${n * c}`
+  item.value = `tab${c}`
+    tab.appendChild(item)
+
+}
+
+}
