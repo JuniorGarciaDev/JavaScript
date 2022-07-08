4c24834530bfe7c7b588ed7828d4a858993c754d
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2022-07-08 14:24:30 -0300

    aula 16 ex 18 1.0

---
+++ aulas/aula16ex/ex18/script.js
@@ -0,0 +1,21 @@
+let num = document.getElementById('fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+}
+
+function inLista(n ){
+
+}
+
+
+function adicionar() {
+    if(isnumeros(num.value) && !inlista(num.value, valores)){
+
+    }else {
+        window.alert('valor invalidade ou ja encontrado na lista ')
+    }
+
+}
