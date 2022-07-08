23d04617544c23b9da35a9ab1ae249fd20ab0943
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2022-07-08 14:24:43 -0300

    analisador de numero desafio

---
+++ desafios/analisador de numeros/script.js
@@ -0,0 +1,13 @@
+function Adicionar(){
+    let res = document.getElementById('res')
+    let textonumero =  document.getElementById('txtn')
+    let num = Number(textonumero.value)
+    let n = []
+    n.push[num.valueOf]
+    console.log(`adicionado ${n}`)
+    
+        res.innerHTML += `o numero ${n[0]} foi adicionado`
+        console.log('adicionado')
+   
+
+}
